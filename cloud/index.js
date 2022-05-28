c44156a918db56aea20da9f954e75223b0104790
Julien Vanier <julien@particle.io>
2022-05-28 15:31:34 -0400

    write sensors to InfluxDB

--- cloud/index.js
+++ cloud/index.js
@@ -1,15 +1,30 @@
 const Influx = require('influx');
 const express = require('express');
+const bodyParser = require('body-parser')
 
-const influx = new Influx.InfluxDB(process.env['INFLUXDB_URL']);
+const influx = new Influx.InfluxDB(process.env['INFLUXDB_URL'] || 'http://localhost:8086/sensors');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Nothing to see here\r\n'));
 
-app.post('/hook', (req, res) => {
+app.post('/hook', async (req, res) => {
+	try {
+		console.log('Received hook', req.body.data);
 
-	res.json({ ok: true });
+		const data = JSON.parse(req.body.data);
+
+		await influx.writePoints([{
+			measurement: 'vehicle',
+			fields: data
+		}]);
+
+		res.json({ ok: true });
+	} catch (error) {
+		console.error('Error processing hook', error);
+		res.status(500).json({ ok: false });
+	}
 });
 
 
